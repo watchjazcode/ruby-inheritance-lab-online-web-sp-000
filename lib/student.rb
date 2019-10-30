036274b7efe6207e6c034c85a93f0efea844be8d
Jazmin M <jazminmerckel@gmail.com>
2019-10-30 00:32:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
 class Student < User
+  
+  attr_accessor :knowledge
+
+  def initialize(knowledge)
+    @knowledge = []
+  end
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 
 end
